b6149e2cf7fe5e41e47cd35b295a15415f398637
proactive-callee-1012 <shaemc22@gmail.com>
2020-12-23 02:03:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ function divide(a,b){
   return a / b;
 }
 function increment(n){
-  return 
+  return n + 1;
+}
+function decrement(n){
+  return n - 1
 }
 function makeInt(n){
   return parseInt(n, 10);
