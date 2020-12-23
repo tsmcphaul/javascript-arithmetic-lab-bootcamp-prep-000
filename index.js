9dea6eec119972bca6ab73108ff6381a297364cb
proactive-callee-1012 <shaemc22@gmail.com>
2020-12-23 01:39:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var a, b;
 a = Math.floor(Math.random() * 1000);
 b = Math.floor(Math.random() * 1000);
 
-function add(){
-  a + b;
+function add(a,b){
+  return a + b;
 }
 function subtract(){
-  a - b;
+  return a - b;
 }
 function multiply(){
-  a * b;
+  return a * b;
 }
